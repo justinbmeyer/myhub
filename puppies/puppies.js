a76de74e7afd872e8cf10eecd4fd056ec0e98fe9
Justin Meyer <justinbmeyer@gmail.com>
2017-08-30 19:28:28 -0700

    broken myhub

---
+++ puppies/puppies.js
@@ -0,0 +1,27 @@
+require("justifiedGallery");
+var $ = require("jquery");
+
+module.exports = function(selector) {
+    $(selector).html("Loading...");
+
+    $.ajax({
+        url: 'https://api.flickr.com/services/feeds/photos_public.gne',
+        dataType: 'jsonp',
+        jsonpCallback: "jsonFlickrFeed",
+        data: {
+            "tags": "puppy",
+            "format": "json"
+        },
+        success: function(response) {
+            var html = response.items.map(function(item, index) {
+                return '<a href="' + item.link + '">' +
+                    '<img alt="' + item.title + '" src="' + item.media.m + '"/>' +
+                    '</a>';
+            }).join("");
+            var root = $("<div>").html(html);
+
+            $(selector).html(root);
+            root.justifiedGallery();
+        }
+    });
+};
---
+++ weather/weather.js
@@ -0,0 +1,34 @@
+import $ from "jquery";
+
+function toClassName(txt) {
+  return txt.toLowerCase().replace(/ /g, "-");
+}
+
+export default function(selector){
+    var city = "sanjose ca";
+    $(selector).html("Loading...");
+    $.ajax({
+        url: `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places%20where%20text%3D%22${city}%22)&format=json&diagnostics=true&callback=`,
+    }).then(function(response){
+      var weather = response.query.results.channel;
+      var forecast = weather.item.forecast;
+      var defs = forecast.map(function(day){
+        return `
+          <li>
+            <span class="date">${day.date}</span>
+            <span class="description ${toClassName(day.text)}">${day.text}</span>
+            <span class="high-temp">${day.high}<sup>&deg;</sup></span>
+            <span class="low-temp">${day.low}<sup>&deg;</sup></span>
+          </li>
+        `;
+      });
+      $(selector).html(`
+        <div class="forecast">
+          <ul>
+            ${defs.join("")}
+          </ul>
+        </div>
+      `);
+    });
+
+}
